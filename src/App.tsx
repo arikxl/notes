b36a4aea812b5d8a87115a28e90d2138b7759bd4
Arik Alexandrov <arikxl@gmail.com>
2022-11-15 22:11:26 +0200

    comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route, Navigate } from "react-router-dom"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from 'react-bootstrap';
+import NewNote from './components/NewNote';
 
 function App() {
 
-  return (
-    <div className="App">
-      
-    </div>
+  return ( 
+    <Container className="my-4 ">
+      <Routes>
+        <Route path="/" element={ <h1>HOME</h1>} />
+        <Route path="/new" element={ <NewNote/>} />
+        <Route path="/:id" >
+            <Route index element={ <h1>Show</h1>} />
+            <Route path="edit" element={ <h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" /> }/>
+      </Routes>
+    </Container>
   )
 }
 
---
+++ src/components/NewNote.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NoteForm from './NoteForm'
+
+type Props = {}
+
+const NewNote = (props: Props) => {
+  return (
+    <>
+        <h1 className="mb-4" > New Note</h1>
+        <NoteForm />
+        
+    </>
+  )
+}
+
+export default NewNote
